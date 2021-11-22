e00a3906cd3d176fc33479e655ce46b9ee7cf61a
peterr4405 <48211875+peterr4405@users.noreply.github.com>
2021-11-22 21:44:11 +0800

    Add files via upload

---
+++ Leet_code_2021/src/leet_290.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class leet_290 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		wordPattern("abba","dog cat cat dog");
+	}
+    public static boolean wordPattern(String pattern, String s) {
+        
+        String[] s1 = s.split(" ");
+        if(pattern.length() != s1.length) return false;
+        
+        Map<Character,String> map = new HashMap();
+        
+        for(int i=0;i < s1.length;i++){
+            
+            if(map.containsKey(pattern.charAt(i))){
+               if(!map.get(pattern.charAt(i)).equals(s1[i])){
+                   return false;
+               }
+            }else{
+            	if(map.containsValue(s1[i])) {
+            		return false;
+            	}
+                map.put(pattern.charAt(i),s1[i]);
+            }
+            
+        }
+        
+        return true;
+    }
+}
